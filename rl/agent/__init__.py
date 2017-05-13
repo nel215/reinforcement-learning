0fe357569b65fce802092ce318e95785bfea32c7
nel215 <otomo.yuhei@gmail.com>
2017-05-14 05:31:19 +0900

    Separate main flow

--- rl/agent/__init__.py
+++ rl/agent/__init__.py
@@ -61,16 +61,17 @@ def run_episode(agent):
     return cnt
 
 
-trial = 50
-n_episode = 2000
-result = []
-for i in range(trial):
-    print('trial %i start.' % (i))
-    agent = Agent(env.action_space)
-    for j in range(n_episode):
-        cnt = run_episode(agent)
-        result.append([j, cnt])
+if __name__ == '__main__':
+    trial = 50
+    n_episode = 2000
+    result = []
+    for i in range(trial):
+        print('trial %i start.' % (i))
+        agent = Agent(env.action_space)
+        for j in range(n_episode):
+            cnt = run_episode(agent)
+            result.append([j, cnt])
 
-df = pd.DataFrame(result, columns=['episode', 'n_move'])
-df.plot.scatter(x='episode', y='n_move')
-plt.savefig('result.png')
+    df = pd.DataFrame(result, columns=['episode', 'n_move'])
+    df.plot.scatter(x='episode', y='n_move')
+    plt.savefig('result.png')
